fa5155a6b8b6eb9db4d07a8f1c79602aa19ff5e7
Setayesh Khasehtarash <setakhaseh@gmail.com>
2025-10-27 16:52:38 +0330

    Update data_collector.py

--- data_collector.py
+++ data_collector.py
@@ -21,7 +21,7 @@ IM_HEIGHT= 480
 
 def process_img(image):
     i=np.array(image.raw_data)
-    print(i.shape)
+    #print(i.shape)
     i2=i.reshape((IM_HEIGHT,IM_WIDTH,4))
     i3=i2[:, :, :3]
     cv2.imshow("",i3)
@@ -33,28 +33,35 @@ try:
     client = carla.Client('localhost', 2000)
     client.set_timeout(10)
     world=client.get_world()
+
     blueprint_library=world.get_blueprint_library()
-    
+    #vehicle_bp -> bp
     bp=blueprint_library.filter("model3")[0]
-    print(bp)
+    #for locating in specific area-> transform=world.get_map().get_spawn_points()[75]
 
     spawn_point=random.choice(world.get_map().get_spawn_points())
-
     vehicle= world.spawn_actor(bp, spawn_point)
+    #...........................................
+
 
     vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
     actor_list.append(vehicle)
+
+    #adding other sesnors is in the same way
     cam_bp=blueprint_library.find("sensor.camera.rgb")
     cam_bp.set_attribute("image_size_x",f"{IM_WIDTH}")
     cam_bp.set_attribute("image_size_y",f"{IM_HEIGHT}")
-    cam_bp.set_attribute("fov","110")
+    cam_bp.set_attribute("fov","110")#normally 90
 
     spawn_point= carla.Transform(carla.Location(x=2.5,z=0.7))
 
     sensor= world.spawn_actor(cam_bp, spawn_point, attach_to= vehicle)
+    if not os.path.exists("output\\"):
+        os.mkdir("output\\")
     actor_list.append(sensor)
+    #sensor.listen(lambda data: data.save_to_disk('output/%.6d.jpg' % data.frame))
     sensor.listen(lambda data: process_img(data))
-    
+    vehicle.set_autopilot(True)
     time.sleep(5)
 
 finally:
