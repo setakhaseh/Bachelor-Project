1ce32b3bf5d2f790fe367a6cc1518503741a38d4
Setayesh Khasehtarash <setakhaseh@gmail.com>
2025-10-27 14:03:23 +0330

    Update data_collector.py

--- data_collector.py
+++ data_collector.py
@@ -14,13 +14,19 @@ import random
 import time
 import numpy as np
 import carla
+import cv2
 
 IM_WIDTH= 640
 IM_HEIGHT= 480
 
 def process_img(image):
     i=np.array(image.raw_data)
-    print(dir(image))
+    print(i.shape)
+    i2=i.reshape((IM_HEIGHT,IM_WIDTH,4))
+    i3=i2[:, :, :3]
+    cv2.imshow("",i3)
+    cv2.waitKey(1)
+    return i2/255.0
 
 actor_list = []
 try: 
@@ -40,7 +46,7 @@ try:
     actor_list.append(vehicle)
     cam_bp=blueprint_library.find("sensor.camera.rgb")
     cam_bp.set_attribute("image_size_x",f"{IM_WIDTH}")
-    cam_bp.set_attribute("image_size_y",f"{IM_WIDTH}")
+    cam_bp.set_attribute("image_size_y",f"{IM_HEIGHT}")
     cam_bp.set_attribute("fov","110")
 
     spawn_point= carla.Transform(carla.Location(x=2.5,z=0.7))
